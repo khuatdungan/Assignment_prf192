e341e2131d7a91311b6b4ab5f9ffaa06df03c1af
huyhao27 <89589680+huyhao27@users.noreply.github.com>
2024-07-08 21:27:38 +0700

    define SIM
    create new type var is SIM by use typedef struct

---
+++ Assignment_PRF192_prj5.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+// ***** dinh nghia bien SIM *****
+typedef struct {
+    char cccd[20];
+    char lastName[50];
+    char firstName[50];
+    char network[50];
+    char phoneNumber[15];
+    float price;
+} SIM;
+
+
+
+
+
+
+
+
+
+
+int main()
+{
+
+	return 0;
+}
